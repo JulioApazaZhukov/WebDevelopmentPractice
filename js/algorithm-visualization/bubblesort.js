56313b606588bcc6980669aa8b93c4742ea13887
Julio Apaza <79545504+JulioApazaZhukov@users.noreply.github.com>
2025-09-01 19:04:17 -0500

    09-01-2025

--- js/algorithm-visualization/bubblesort.js
+++ js/algorithm-visualization/bubblesort.js
@@ -1,27 +1,38 @@
 'use strict';
 
-//let arr = new Array(1, 25, 3, 54, 8, 9, 65);
-let arr = Int16Array.of(1, 25, 3, 54, 8, 9, 65);
-console.log(arr);
-
-for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-        let aux;
-        if (arr[j] > arr[j + 1]) {
-            aux = arr[j];
-            arr[j] = arr[j + 1];
-            arr[j + 1] = aux;
+function bubblesort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            let aux;
+            if (arr[j] > arr[j + 1]) {
+                aux = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = aux;
+            }
         }
     }
+    return arr;
 }
+
+let arr = new Array(1, 25, 3, 54, 8, 9, 65);
+console.log(arr);
+
+arr = bubblesort(arr);
 
 console.log(arr);
 
-// BigInt: 123456789123456798123456798n
-const num = 900719925474099549494848n;
-const num2 = 900719925474095494948485656n;
-console.log(num+num2);
-let n = undefined;
+var utils = [
+   function(a, b) { return a + b; },
+   function(a, b) { return a - b; }
+];
 
-let usern = prompt("Whats your name?", "");
-alert(`Your name is ${usern}`);
+console.log(utils[0](10, 20));
+console.log(utils[1](50, 5));
+
+var utils2 = {
+   add: function(a, b) { return a + b; },
+   diff: function(a, b) { return a - b; }
+}
+
+console.log(utils2.add(10, 20));
+console.log(utils2.diff(50, 5));
